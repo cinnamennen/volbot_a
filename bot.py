89038d542090c2078be24efc53fdba7e6078874b
Tyler Stuessi <computernerdforlife49@gmail.com>
2015-09-08 11:58:52 -0400

    Added function to show disambiguation options in tellmeabout

--- bot.py
+++ bot.py
@@ -202,6 +202,11 @@ class TestBot(irc.bot.SingleServerIRCBot):
             #3 sentence limit. Can be extended later
             summary = wikipedia.summary(query, 3)
             self.privmsg(channel, summary)
+        except wikipedia.exceptions.DisambiguationError as e:
+            op_list = e.options
+            message = "This could refer to a lot of things:\n"
+            message += "\n".join(op_list)
+            self.privmsg(channel, message)
         except wikipedia.exceptions.WikipediaException:
             self.privmsg(channel, "Sorry, can't find that.")
 
