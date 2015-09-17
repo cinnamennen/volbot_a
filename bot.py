5cf76a6b01867e3f29e76073ce0ce905feaa601d
Joseph Connor <josephc346@gmail.com>
2015-09-16 23:26:10 -0400

    Add stop words (hardcoded, for now)

--- bot.py
+++ bot.py
@@ -301,11 +301,27 @@ class VolBot(irc.bot.SingleServerIRCBot):
             counter.update([clean(word) for word in doc['message'].split()])
         # total number of words from user.
         wc = sum(counter.itervalues())
+        stop = set([
+            'ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about',
+            'once', 'during', 'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be',
+            'some', 'for', 'do', 'its', 'yours', 'such', 'into', 'of', 'most', 'itself', 'other',
+            'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the', 'themselves',
+            'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me', 
+            'were', 'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 
+            'above', 'both', 'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 
+            'before', 'them', 'same', 'and', 'been', 'have', 'in', 'will', 'on', 'does', 
+            'yourselves', 'then', 'that', 'because', 'what', 'over', 'why', 'so', 'can', 'did',
+            'not', 'now', 'under', 'he', 'you', 'herself', 'has', 'just', 'where', 'too', 'only',
+            'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 't', 'being', 'if', 'theirs',
+            'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than'
+        ])
+
+        favorites = [(w,c) for w, c in counter.most_common() if w not in stop][:10]
 
         self.privmsg(channel, "%s sent %d messages of %d logged messages (%.2f%%)" % 
             (nick, nick_count, all_count, percent))
         self.privmsg(channel, "Used %d unique words. Favorites: %s" % (wc, ', '.join(
-            "%s (%.2f%%)" % (w,(float(c)/wc)) for w,c in counter.most_common(10)
+            "%s (%.2f%%)" % (w,(float(c)/wc)) for w,c in favorites
         )))
 
 
